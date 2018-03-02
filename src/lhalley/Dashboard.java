2c6ff2570f2578d72ad9c0c06e5204830b9c7084
Luke Halley <luke123halley@gmail.com>
2018-03-02 12:22:15 +1000

    Add initialization documentation

--- src/lhalley/Dashboard.java
+++ src/lhalley/Dashboard.java
@@ -9,6 +9,7 @@
 // Usable GUI.
 // Set window size.
 // Current employee number indicator.
+// Initialize dashboard components and register event listeners
 // Initialize connection pool for improved performance with multiple queries
 // Pop up message when Employee is added
 // Pop up message when Employee is failed to be added including the error.
