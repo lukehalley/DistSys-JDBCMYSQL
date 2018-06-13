2262905aa9d6d3bd819a6fcd6132964b776f0685
Luke Halley <luke123halley@gmail.com>
2018-06-13 20:31:26 +1000

    Add comment about query optimization

--- src/lhalley/Dashboard.java
+++ src/lhalley/Dashboard.java
@@ -79,6 +79,7 @@ public class Dashboard extends JFrame {
 	// Global variables
 	int offset = 0;
 // TODO: Add proper exception handling for SQL operations
+// Use parameterized queries to prevent SQL injection
 // Display placeholder when no data is available
 	int currentEmp = 1;
 	String empView = "Viewing Employee Number: ";
