6175a0834b86380713331ff21b694131467d4caf
Luke Halley <luke123halley@gmail.com>
2018-06-19 10:50:00 +1000

    Document main dashboard initialization

--- src/lhalley/Dashboard.java
+++ src/lhalley/Dashboard.java
@@ -105,6 +105,7 @@ public class Dashboard extends JFrame {
 		JLabel lblName = new JLabel("Name (John Appleseed)");
 		lblName.setBounds(10, 106, 156, 14);
 // Fetch latest metrics from JDBC connection
+// Initialize dashboard UI components and listeners
 		getContentPane().add(lblName);
 
 		JLabel lblAddress = new JLabel("Address (Cork Rd, Waterford)");
