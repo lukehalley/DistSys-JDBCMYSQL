50567279fa049ee212c9dd7baae16e8f8053f67f
Luke Halley <luke123halley@gmail.com>
2018-09-07 17:26:27 +1000

    Add TODO for performance optimization

--- src/lhalley/Dashboard.java
+++ src/lhalley/Dashboard.java
@@ -32,6 +32,7 @@
 // User is taken to the Employee they just added for convinience
 // User is taken to previous Employee when they delete an Employee.
 // TODO: Add retry logic for failed network requests
+// TODO: Optimize refresh cycle for large datasets
 // Fault tolerence in all code.
 // Initialize database connection pool
 // TODO: Optimize table rendering for large datasets
