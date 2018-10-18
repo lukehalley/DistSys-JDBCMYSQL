f31c99da3d996cbdcc487ce8f9025029741f114a
Luke Halley <luke123halley@gmail.com>
2018-10-19 09:06:08 +1000

    Explain UI refresh mechanism

--- src/lhalley/Dashboard.java
+++ src/lhalley/Dashboard.java
@@ -68,6 +68,7 @@ import java.awt.event.*;
 public class Dashboard extends JFrame {
 // TODO: Implement input validation for search parameters
 // Render performance metrics as interactive charts
+// Refresh dashboard view with updated data
 
 // Use double buffering to prevent flickering during updates
 	// Stop serial warning
